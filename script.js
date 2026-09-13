d9325d4aa98947d07ff7d1aa13627853e8677734
Tetyanna Tan <tetyannatannn@gmail.com>
2026-09-13 20:43:01 +0700

    Update Happy Mail

---
+++ script.js
@@ -0,0 +1,37 @@
+const form = document.getElementById("orderForm");
+
+const orderTableBody = document.getElementById("orderTableBody");
+
+form.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const penerima = document.getElementById("penerima").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const mailType = document.getElementById("mail-type").value;
+    const area = document.getElementById("area").value;
+    const date = document.getElementById("date").value;
+    const message = document.getElementById("message").value;
+    const request = document.getElementById("request").value;
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+        <td>${name}</td>
+        <td>${penerima}</td>
+        <td>${email}</td>
+        <td>${phone}</td>
+        <td>${mailType}</td>
+        <td>${area}</td>
+        <td>${date}</td>
+        <td>${message}</td>
+        <td>${request}</td>
+    `;
+
+    orderTableBody.appendChild(row);
+
+    form.reset();
+
+});
